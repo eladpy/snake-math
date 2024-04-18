c107c34ff260eb457843d557b3d00e9f85cacab8
mimimi <bionicrooster4242@gmail.com>
2024-04-18 14:11:07 +0300

    nghjhgj

--- logic/math.py
+++ logic/math.py
@@ -1,4 +1,32 @@
 from Constants import *
 import pygame
+from random import randint
 
-peepeepoopoo
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 + num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+def math():
+    num1 = randint(0, 10)
+    num2 = randint(0, 10)
+    function_list = [add, subtract, multiply, divide]
+    random = randint(0, 4)
+    answer = function_list[random](num1, num2)
+
+    # list[num1, num2, answer, start, end]
+    # for index in range()
+    return [num1, num2, answer, ]
+
