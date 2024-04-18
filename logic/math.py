7002e04946821ec5611e5495238aa4e6f9f8e38f
mimimi <bionicrooster4242@gmail.com>
2024-04-18 14:46:12 +0300

    May god help us all

--- logic/math.py
+++ logic/math.py
@@ -20,12 +20,23 @@ def divide(num1, num2):
 
 
 def math():
-    num1 = randint(0, 10)
-    num2 = randint(0, 10)
+    num1 = randint(0, 100)
+    num2 = randint(0, 100)
     function_list = [add, subtract, multiply, divide]
-    random = randint(0, 4)
+    random = randint(0, 3)
     answer = function_list[random](num1, num2)
     bottom_range = answer - 10
     top_range = answer + 10
-    return [num1, num2, answer, bottom_range, top_range]
+    return [num1, num2, answer, bottom_range, top_range, function_list[random]]
 
+
+def to_string(num1, num2, operator):
+    if operator == add:
+        operator = "+"
+    elif operator == subtract:
+        operator = "-"
+    elif operator == multiply:
+        operator = "*"
+    else:
+        operator = "/"
+    return f'{num1} {operator} {num2} = ?'
